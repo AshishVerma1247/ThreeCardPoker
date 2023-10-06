34482f41edc3480e680f3506996b77880d9c68c6
Ashish Verma <139619987+AshishVerma1247@users.noreply.github.com>
2023-10-07 01:21:01 +0530

    Commit Controller

--- src/main/java/com/ashish/ThreeCardPoker/controller/GameController.java
+++ src/main/java/com/ashish/ThreeCardPoker/controller/GameController.java
@@ -1,4 +1,15 @@
 package com.ashish.ThreeCardPoker.controller;
 
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GameController {
+    @RequestMapping("/hello")
+    public String Calc(Model model)
+    {
+        return "helloworld";
+    }
 }
